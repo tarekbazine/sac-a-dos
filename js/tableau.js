f45569cb72b80b1d0f188b3ea54cf56d49f5df2d
Mahfoud047 <em_chikhaissa@esi.dz>
2017-10-24 00:03:04 +0100

    some modification in home view

--- js/tableau.js
+++ js/tableau.js
@@ -1,7 +1,7 @@
  $(document).ready(function(){
       var i=1;
      $("#add_row").click(function(){
-      $('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='value"+i+"' type='text' placeholder='valeur' class='form-control input-md'  /> </td><td><input  name='weight"+i+"' type='text' placeholder='poid'  class='form-control input-md'></td>");
+      $('#addr'+i).html("<td>"+ "objet"+(i+1) +"</td><td><input name='value"+i+"' type='text' placeholder='valeur' class='form-control input-md'  /> </td><td><input  name='weight"+i+"' type='text' placeholder='poid'  class='form-control input-md'></td>");
 
       $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
       i++; 
