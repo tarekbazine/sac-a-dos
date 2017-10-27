bb3601c8eb725a792182282761e2a122888fb561
terryBaz <et_bazine@esi.dz>
2017-10-28 00:00:39 +0100

    link the algo detail page

--- js/algo.js
+++ js/algo.js
@@ -5,85 +5,75 @@
 
 
 function max(a, b) {
-  if (a > b) return a;
-  return b;
+    if (a > b) return a;
+    return b;
 }
 
-//those are going to be inputs from user
-var N = 4; //5
-var W = 7; //9
+function optimaleSolution(W, N, w, v) {
 
-/*there is another seggestion is to order the items by the weight asc
-so the last step of gathering those objects is more easier !!*/
-
-/*algorithme's sructures*/
-var v = [1,
-  4,
-  5,
-  7
-];
-
-var w = [
-  1,
-  3,
-  4,
-  5
-];
-//    int v[] = {0  ,8, 2, 7, 6, 4};
-//    int w[] = {0  ,1, 3, 6, 7, 2};
-var mat = [];
+    var mat = [];
 
 // js array two dimension building
-for (var j = 0; j <= W; j++) {
-  mat[j] = [];
-}
+    for (var j = 0; j <= W; j++) {
+        mat[j] = [];
+    }
 
-/*init matrix*/
-for (var i = 0; i < N; i++) {
-  mat[i][0] = 0;
-}
+    /*init matrix*/
+    for (var i = 0; i < N; i++) {
+        mat[i][0] = 0;
+    }
 
 //this an optimization ???!! we deleting a not-needed row
-for (var j = 1; j <= W; j++) {
-  if (w[0] > j) {
-    mat[0][j] = 0;
-  } else {
-    mat[0][j] = w[0];
-  }
-}
-
-
-for (var i = 1; i < N; i++) {
-  for (var j = 0; j <= W; j++) {
-    if (w[i] > j) {
-      mat[i][j] = mat[i - 1][j];
-    } else {
-      mat[i][j] = max(v[i] + mat[i - 1][j - w[i]], mat[i - 1][j]);
+    for (var j = 1; j <= W; j++) {
+        if (w[0] > j) {
+            mat[0][j] = 0;
+        } else {
+            mat[0][j] = v[0];
+        }
     }
-  }
-}
+
+
+    for (var i = 1; i < N; i++) {
+        for (var j = 0; j <= W; j++) {
+            if (w[i] > j) {
+                mat[i][j] = mat[i - 1][j];
+            } else {
+                mat[i][j] = max(v[i] + mat[i - 1][j - w[i]], mat[i - 1][j]);
+            }
+        }
+    }
 
 //printing results
 //matrix
-for (var i = 0; i < N; i++) {
-  for (var j = 0; j <= W; j++) {
-    console.log(mat[i][j]);
-  }
-  console.log("\n");
-}
+    for (var i = 0; i < N; i++) {
+        for (var j = 0; j <= W; j++) {
+            console.log(mat[i][j]);
+        }
+        console.log("\n");
+    }
 
-//    //the optimal weight
-console.log("\n\nthe optimal weight " + mat[N - 1][W] + " \n\n");
+//    //the optimal gain
+    console.log("\n\nthe optimal gain " + mat[N - 1][W] + " \n\n");
 
 //list of the selected objects
-var j = W;
-for (var i = N - 1; i > 0; i--) {
-  if ((mat[i][j] == 0)) {
-    console.log("fin");
-    break;
-  }
-  if ((mat[i][j] != mat[i - 1][j])) {
-    console.log("object num " + (i + 1) + " has been selected \n");
-    j = j - w[i];
-  }
+    var selectedOject=[];
+    var j = W;
+    for (var i = N - 1; i > 0; i--) {
+        if ((mat[i][j] == 0)) {
+            console.log("fin");
+            break;
+        }
+        if ((mat[i][j] != mat[i - 1][j])) {
+            selectedOject.push(i);
+            console.log("object num " + (i + 1) + " has been selected \n");
+            j = j - w[i];
+        }
+    }
+    if ((mat[0][j] != 0)){
+        selectedOject.push(0);
+        console.log("object num 0 has been selected \n");
+    }
+
+    return selectedOject;
 }
+
--- js/tableau.js
+++ js/tableau.js
@@ -1,69 +1,88 @@
- 
 // The only number function
- jQuery('.numbersOnly').keyup(function () { 
-    this.value = this.value.replace(/[^0-9\.]/g,'');
+jQuery('.numbersOnly').keyup(function () {
+    this.value = this.value.replace(/[^0-9\.]/g, '');
 });
 
 // Add/delete rows from table
- $(document).ready(function(){
-    var k=0;
+$(document).ready(function () {
+    var k = 0;
 
-     $("#add_row").click(function(e){
+    $("#add_row").click(function (e) {
         k++;
-         added_row="<td>"+ "objet"+(k+1) +"</td><td><input name='value"+k+"' type='text' placeholder='valeur' class='form-control input-md'  /> </td><td><input  name='weight"+k+"' type='text' placeholder='poid'  class='form-control input-md'></td>";
- 
-         $('#tab_logic tbody:last-child').append('<tr id="addr'+(k)+'">'+added_row+'</tr>');
-         
-        });
+        added_row = "<td>" + "objet" + (k + 1) + "</td><td><input name='value" + k + "' type='text' placeholder='valeur' class='form-control input-md'  /> </td><td><input  name='weight" + k + "' type='text' placeholder='poid'  class='form-control input-md'></td>";
+
+        $('#tab_logic tbody:last-child').append('<tr id="addr' + (k) + '">' + added_row + '</tr>');
+
+    });
 
 
-     $("#delete_row").click(function(e){
-       if (k>0){
-           $("#addr"+(k)).remove();
-           k--;
-     };
-   });
+    $("#delete_row").click(function (e) {
+        if (k > 0) {
+            $("#addr" + (k)).remove();
+            k--;
+        }
+        ;
+    });
 
 });
 
 
 //Wmax
- var W;
+var W;
 //Nbr objects
- var N;
+var N;
 // Data rows
- var v = []; 
- var w = [];
+var v = [];
+var w = [];
 
 
 var submit = $('#btn-submit'); // Get the close button
-submit.click(function(e){
+submit.click(function (e) {
 
 //Get Wmax;
-W=$("input[name='vmax']").val();
+    var W = parseInt($("input[name='vmax']").val());
 
 
-  var table = $('#tab_logic > tbody').children();
+    var table = $('#tab_logic > tbody').children();
 
 //Get nb objects
-N=table.length;
+    var N = table.length;
+
+    var v = [], w = [];
 
 // Get table dataSSSS
-   table.each(function (i, el){
-            value = $(this).children().eq(1).children().eq(0).val(),
+    table.each(function (i, el) {
+        value = $(this).children().eq(1).children().eq(0).val(),
             weight = $(this).children().eq(2).children().eq(0).val();
-            v.push(value);
-            w.push(weight);
-            
-           
-  });
- console.log('v='+v);
- console.log('w='+w);
- console.log(W);
- console.log(N);
+        v.push(parseInt(value));
+        w.push(parseInt(weight));
 
- // set arrays empty
- v=[];
- w=[];
+
+    });
+
+    console.log('v=' + v);
+    console.log('w=' + w);
+    console.log(W);
+    console.log(N);
+
+    var selectedOject = optimaleSolution(W, N, w, v);
+
+    console.log('selectedOject=' + selectedOject);
+
+    for (var i = 0; i < N; i++) {
+        if (selectedOject.indexOf(i) == (-1)) {
+            $('#addr' + i + " td:first").css("background-color", "#d9534f").css("color", "white");
+        } else {
+            $('#addr' + i + " td:first").css("background-color", "#5cb85c").css("color", "white");
+        }
+    }
+
+    //cookies to send them to the detail page
+    $.cookie('v', JSON.stringify(v), {expires: 2});
+    $.cookie('w', JSON.stringify(w), {expires: 2});
+    $.cookie('N', N, {expires: 2});
+    $.cookie('W', W, {expires: 2});
+
+
 });
 
